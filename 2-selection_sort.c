80d274e65076db84229742f737641803ba033276
mokasp <kasperbouldin@gmail.com>
2023-08-21 23:34:39 -0700

    added initial selection sort files

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t step, min, i;
+
+	for (step = 0; step < size - 1; step++)
+	{
+		min = step;
+		for (i = step + 1; i < size; i++)
+		{
+			if (array[i] < array[min])
+				min = i;
+		}
+		swap(&array[min], &array[step]);
+		print_array(array, size);
+	}
+}
+
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
--- sort.h
+++ sort.h
@@ -21,5 +21,9 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void sortedInsert(listint_t **list, listint_t *newNode);
+void selection_sort(int *array, size_t size);
+void swap(int *a, int *b);
 
 #endif
